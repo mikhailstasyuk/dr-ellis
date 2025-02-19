0790b9afb058979b20e1fbac137d9aaeee39ea4f
Mikhail Stasyuk <mikhail.stasyuk@gmail.com>
2025-02-19 11:28:47 +0400

    Fix code formatting

--- src/dr_ellis/app.py
+++ src/dr_ellis/app.py
@@ -1,15 +1,16 @@
 import asyncio
 import os
-from logger_config import logger
+
 from dotenv import load_dotenv
 from groq import Groq, APIError
+from logger_config import logger
 from telebot.async_telebot import AsyncTeleBot
 
 load_dotenv()
 
 
-bot = AsyncTeleBot(os.getenv('BOT_TOKEN'))
-client = Groq(api_key=os.environ['GROQ_API_KEY'])
+bot = AsyncTeleBot(os.getenv("BOT_TOKEN"))
+client = Groq(api_key=os.environ["GROQ_API_KEY"])
 
 
 def get_response(message):
@@ -22,7 +23,7 @@ def get_response(message):
                         "content": message
                     }
                 ],
-                model='llama3-70b-8192'
+                model="llama3-70b-8192"
             )
         response = chat_completion.choices[0].message.content
         logger.info(f"Received response from Groq API: {response}")
@@ -34,7 +35,7 @@ def get_response(message):
             401: "Unauthorized. Check Groq API key.",
             404: "Model not found. Verify the model name.",
             429: "Rate limit exceeded. Try again later.",
-            500: "Internal server error. Try again later."
+            500: "Internal server error. Try again later.",
         }
         logger.error(f"API error: {e}")
         return error_messages.get(e.status_code, f"API error: {e}")
@@ -44,19 +45,26 @@ def get_response(message):
         return f"An unexpected error occurred: {str(e)}"
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 async def start(message):
-    greeting_msg = 'Hello, I am Dr. Ellis. How can I help you today?'
+    greeting_msg = "Hello, I am Dr. Ellis. How can I help you today?"
     await bot.send_message(message.chat.id, greeting_msg)
     logger.info(f"User {message.chat.id} started a conversation.")
 
 
 @bot.message_handler(func=lambda message: True, content_types=[
-    'audio', 'photo', 'voice', 'video', 'document',
-    'location', 'contact', 'sticker'
-])
+    "audio",
+    "photo",
+    "voice",
+    "video",
+    "document",
+    "location",
+    "contact",
+    "sticker",
+    ],
+)
 async def handle_non_text(message):
-    info_msg = 'I can only process text messages for now.'
+    info_msg = "I can only process text messages for now."
     await bot.reply_to(message, info_msg)
     logger.info(f"User {message.chat.id} sent a non-text message.")
 
@@ -67,5 +75,6 @@ async def handle_text(message):
     response = get_response(message.text)
     await bot.reply_to(message, response)
     logger.info(f"Dr. Ellis replied to user {message.chat.id}: {response}")
+
 
 asyncio.run(bot.polling())
--- src/dr_ellis/logger_config.py
+++ src/dr_ellis/logger_config.py
@@ -1,13 +1,13 @@
 import logging
 import os
 
-if not os.path.exists('logs'):
-    os.makedirs('logs')
+if not os.path.exists("logs"):
+    os.makedirs("logs")
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    filename='logs/bot.log'
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename="logs/bot.log",
 )
 
-logger = logging.getLogger('DrEllsiBot')
+logger = logging.getLogger("DrEllsiBot")
